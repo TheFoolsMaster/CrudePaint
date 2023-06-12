9e8ef165495de96cf3b7ede8445173e973bbcb43
TheFoolsMaster <95000621+TheFoolsMaster@users.noreply.github.com>
2023-06-12 12:46:50 +0200

    Basic painting functionality and change of colour

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,22 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
+            drawingBoard = new Panel();
+            btn_PenColor = new Button();
             SuspendLayout();
             // 
-            // panel1
+            // drawingBoard
             // 
-            panel1.Location = new Point(320, 103);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(374, 208);
-            panel1.TabIndex = 0;
+            drawingBoard.Location = new Point(320, 103);
+            drawingBoard.Name = "drawingBoard";
+            drawingBoard.Size = new Size(374, 208);
+            drawingBoard.TabIndex = 0;
+            drawingBoard.MouseDown += drawingBoard_MouseDown;
+            drawingBoard.MouseMove += drawingBoard_MouseMove;
+            drawingBoard.MouseUp += drawingBoard_MouseUp;
+            // 
+            // btn_PenColor
+            // 
+            btn_PenColor.Location = new Point(35, 58);
+            btn_PenColor.Name = "btn_PenColor";
+            btn_PenColor.Size = new Size(75, 23);
+            btn_PenColor.TabIndex = 1;
+            btn_PenColor.Text = "button1";
+            btn_PenColor.UseVisualStyleBackColor = true;
+            btn_PenColor.Click += btn_PenColor_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(panel1);
+            Controls.Add(btn_PenColor);
+            Controls.Add(drawingBoard);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -51,6 +66,7 @@
 
         #endregion
 
-        private Panel panel1;
+        private Panel drawingBoard;
+        private Button btn_PenColor;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,53 @@
+using System.Drawing;
+
 namespace CrudePaint
 {
     public partial class Form1 : Form
     {
+        private Graphics graphics;
+        private Pen pen;
+        private Point startPoint;
         public Form1()
         {
             InitializeComponent();
+            graphics = drawingBoard.CreateGraphics();
+            pen = new Pen(Color.Black);
+        }
+
+        private void drawingBoard_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                drawingBoard.Capture = true;
+                startPoint = e.Location;
+            }
+        }
+
+        private void drawingBoard_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                graphics.DrawLine(pen, startPoint, e.Location);
+                startPoint = e.Location;
+            }
+        }
+
+        private void drawingBoard_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                drawingBoard.Capture = false;
+            }
+        }
+
+        private void btn_PenColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                pen.Color = colorDialog.Color;
+                btn_PenColor.BackColor = colorDialog.Color;
+            }
         }
     }
 }
